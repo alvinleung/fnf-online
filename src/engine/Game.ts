1213aa1c169d98ae720064d7ff73796b788bfe5d
Alvin Leung <alvinleung2009@gmail.com>
2021-04-25 00:15:38 -0700

    Game setup

---
+++ src/engine/Game.ts
@@ -0,0 +1,69 @@
+import * as THREE from "three";
+
+// game engine modules
+import InputSystem, { KeyboardInput, MouseInput } from "./Input";
+
+function init(): HTMLCanvasElement {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  scene.add(camera);
+
+  camera.position.z = 5;
+
+  // add the renderer to html
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // setup input
+  setupInput();
+
+  // start the update function
+  update(renderer, scene, camera);
+
+  return renderer.domElement;
+}
+
+function setupInput() {
+  const keyboard = new KeyboardInput();
+  const mouse = new MouseInput();
+
+  InputSystem.bindAction("left", keyboard.createKeyBinding("KeyA"));
+  InputSystem.bindAction("right", keyboard.createKeyBinding("KeyD"));
+  InputSystem.bindAction("up", keyboard.createKeyBinding("KeyW"));
+  InputSystem.bindAction("down", keyboard.createKeyBinding("KeyS"));
+
+  InputSystem.bindAction("attack", keyboard.createKeyBinding("space"));
+
+  InputSystem.bindAxis("x", keyboard.createAxisBinding("KeyA|KeyD"));
+  InputSystem.bindAxis("y", keyboard.createAxisBinding("KeyW|KeyS"));
+}
+
+function update(
+  renderer: THREE.WebGLRenderer,
+  scene: THREE.Scene,
+  camera: THREE.Camera
+) {
+  if (InputSystem.getAxis("x") !== 0) {
+    console.log("test " + InputSystem.getAxis("x"));
+  }
+
+  // render the scene
+  renderer.render(scene, camera);
+
+  // get ready for next update
+  requestAnimationFrame(() => {
+    update(renderer, scene, camera);
+  });
+}
+
+export default init;
