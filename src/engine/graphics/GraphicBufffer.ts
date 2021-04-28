c138589bb4b9648bf18a12dbde316fa7a3ded2c9
Alvin Leung <alvinleung2009@gmail.com>
2021-04-28 01:23:38 -0700

    Added float support for graphic buffer

--- src/engine/graphics/GraphicBufffer.ts
+++ src/engine/graphics/GraphicBufffer.ts
@@ -4,21 +4,33 @@ export class GraphicBuffer {
   private bufferSize: number;
   private attributePointerLocation: number;
 
-  constructor(
-    gl: WebGLRenderingContext,
-    attributePointerLocation: number,
-    bufferSize: number
-  ) {
+  constructor(gl: WebGLRenderingContext, attributePointerLocation: number) {
     this.buffer = gl.createBuffer();
     this.gl = gl;
     this.attributePointerLocation = attributePointerLocation;
   }
 
   // write data into the buffer
-  public writeBuffer(bufferData: Float32Array, bufferSize: number) {
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, bufferData, this.gl.STATIC_DRAW);
-    this.bufferSize = bufferSize;
+  public writeBuffer(bufferData: Float32Array | number, bufferSize: number) {
+    if (bufferData instanceof Float32Array) {
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
+      this.gl.bufferData(this.gl.ARRAY_BUFFER, bufferData, this.gl.STATIC_DRAW);
+      this.bufferSize = bufferSize;
+      return;
+    }
+
+    if (typeof bufferData === "number") {
+      this.gl.bindBuffer(this.gl.FLOAT, this.buffer);
+      this.gl.bufferData(
+        this.gl.FLOAT,
+        new Float32Array([bufferData]),
+        this.gl.STATIC_DRAW
+      );
+      this.bufferSize = 1;
+      return;
+    }
+
+    console.warn("Cannot write buffer, invalid data type");
   }
 
   // use the buffer for rendering
