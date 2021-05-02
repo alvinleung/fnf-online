0942d5e9049a42e516cf1e71e7aed7e15a1abd1c
Alvin Leung <alvinleung2009@gmail.com>
2021-05-01 17:20:09 -0700

    Cleanup mygame file

--- src/MyGame.ts
+++ src/MyGame.ts
@@ -6,7 +6,6 @@ import {
   ImageRenderer,
   ImageRendererSetup,
 } from "./engine/graphics/Image/ImageRenderer";
-import { RenderingComponent } from "./engine/graphics/RenderingComponent";
 import { RenderingSystem } from "./engine/graphics/RenderingSystem";
 import PlayerControlSystem from "./engine/core/PlayerControlSystem";
 import { PlayerControlComponent } from "./engine/core/PlayerControlComponent";
@@ -14,10 +13,6 @@ import {
   SpriteSheetAnimator,
   SpriteSheetRenderable,
 } from "./engine/graphics/SpriteSheet/SpriteSheetAnimation";
-import {
-  SpriteSheetRenderer,
-  SpriteSheetRendererSetup,
-} from "./engine/graphics/SpriteSheet/old_SpriteSheetRenderer";
 import { TransformComponent } from "./engine/core/TransformComponent";
 import { SoundLoader } from "./engine/assets/SoundLoader";
 
@@ -30,42 +25,6 @@ import { SpriteSheetRenderPass } from "./engine/graphics/SpriteSheet/SpriteSheet
 
 class MyGame extends Game {
   protected gameDidInit() {
-    // ready to go! do some crazy shit here
-    // const testEntity = new Entity();
-
-    // insert and configure component
-    // testEntity.useComponent(TransformComponent);
-    // const renderingComponent = testEntity.useComponent(RenderingComponent);
-    // const imageResource = this.assets.image.get("test");
-    // renderingComponent.setRenderer(new ImageRenderer(imageResource));
-
-    // this.addEntity(testEntity);
-
-    // const spriteSheetAnimator = new SpriteSheetAnimator(
-    //   imageResource,
-    //   6,
-    //   50,
-    //   37
-    // );
-    // spriteSheetAnimator.defineAnimation("idle", 0, 3);
-    // spriteSheetAnimator.defineAnimation("crouch", 4, 7);
-    // spriteSheetAnimator.defineAnimation("run", 8, 13);
-    // spriteSheetAnimator.defineAnimation("jump", 14, 23);
-
-    // spriteSheetAnimator.loop("run");
-
-    // const animatingEntity = new Entity();
-
-    // insert and configure component
-
-    // animatingEntity.useComponent(TransformComponent);
-    // animatingEntity
-    //   .useComponent(RenderingComponent)
-    //   .setRenderer(new SpriteSheetRenderer(spriteSheetAnimator));
-    // animatingEntity.useComponent(PlayerControlComponent);
-
-    // this.addEntity(animatingEntity);
-
     /**
      * Entity 1 - static
      */
@@ -97,6 +56,7 @@ class MyGame extends Game {
       RenderableComponent
     ).renderableObject = new SpriteSheetRenderable(spriteSheetAnimation);
 
+    squareEntity.useComponent(PlayerControlComponent);
     /**
      * Entity 3 - Camera Controller
      */
@@ -104,7 +64,6 @@ class MyGame extends Game {
     const cameraEntity = new Entity();
     cameraEntity.useComponent(TransformComponent).z = 2;
     cameraEntity.useComponent(CameraComponent);
-    cameraEntity.useComponent(PlayerControlComponent);
 
     this.addEntity(cameraEntity);
     this.addEntity(squareEntity2);
