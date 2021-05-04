0937aa354a33bbe2fa2df5990b04cdba58e37ad6
Alvin Leung <alvinleung2009@gmail.com>
2021-05-04 03:04:58 -0700

    Remove dead code

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -1,7 +1,7 @@
 import { Engine } from "./ecs";
 
 // game engine modules
-import InputSystem, { KeyboardInput, MouseInput } from "./input";
+import InputSystem from "./input";
 import { AssetLoader, AssetManager } from "./assets";
 import { RenderingSystem } from "./graphics/RenderingSystem";
 
@@ -113,48 +113,3 @@ export abstract class Game extends Engine {
     });
   }
 }
-
-// function init(): HTMLCanvasElement {
-//   const scene = new THREE.Scene();
-//   const camera = new THREE.PerspectiveCamera(
-//     75,
-//     window.innerWidth / window.innerHeight,
-//     0.1,
-//     1000
-//   );
-
-//   const geometry = new THREE.BoxGeometry();
-//   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//   const cube = new THREE.Mesh(geometry, material);
-//   scene.add(cube);
-//   scene.add(camera);
-
-//   camera.position.z = 5;
-
-//   // add the renderer to html
-//   const renderer = new THREE.WebGLRenderer();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-
-//   // start the update function
-//   update(renderer, scene, camera);
-
-//   return renderer.domElement;
-// }
-
-// function update(
-//   renderer: THREE.WebGLRenderer,
-//   scene: THREE.Scene,
-//   camera: THREE.Camera
-// ) {
-//   if (InputSystem.getAxis("x") !== 0) {
-//     console.log("test " + InputSystem.getAxis("x"));
-//   }
-
-//   // render the game scene
-//   renderer.render(scene, camera);
-
-//   // get ready for next update
-//   requestAnimationFrame(() => {
-//     update(renderer, scene, camera);
-//   });
-// }
