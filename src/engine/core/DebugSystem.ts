ac2b7997b97885a54149dd85d89dd6a15a407d56
kwan <sleepmanleung@gmail.com>
2021-05-05 20:19:30 -0700

    implemented screen space to world interaction
    worked out how to calculate direction based on screen space

--- src/engine/core/DebugSystem.ts
+++ src/engine/core/DebugSystem.ts
@@ -54,11 +54,8 @@ export class DebugSystem extends System {
 
     this.elapsed += delta;
 
-    //transform.position = v3.add(transform.position, v3.create(0,delta,0));
-    //cameraTranform.position = v3.add(cameraTranform.position, v3.create(1,0,0))
-    //console.log(cameraTranform.position);
-
     /*
+    // a slerp application(spin object)
     transform.rotation = q.slerp(
       q.fromAxisAndAngle(q.X_AXIS,0),
       q.fromAxisAndAngle(q.X_AXIS,Math.PI / 2),
@@ -66,37 +63,29 @@ export class DebugSystem extends System {
     );*/
 
     let absoulteX = (game.input.getAxis("yawX") / clientwidth) * 2 - 1;
-    let absoulteY = (game.input.getAxis("yawY") / clientHeight) * 2 - 1;
-    
-    let mousePos = cameraTranform.getPosition();
-    let offset = q.multVec3(
-      q.inverse(cameraTranform.rotation),
-      v3.create(absoulteX, - absoulteY,0)
-    );
-
-    mousePos = v3.add(mousePos, offset)
-    let look = m4.lookAt(transform.getPosition(),mousePos,v3.create(0,1,0));
-    //transform.position = offset;
-    transform.rotation = q.mult( 
-      q.fromAxisAndAngle(q.X_AXIS,Math.PI / 2),
-      q.mat4ToQuatv2(look)
-    );
+    let absoulteY = - ((game.input.getAxis("yawY") / clientHeight) * 2 - 1);
 
     // Vector of the direction the camera is looking at
     let cameraFrontVector = v3.negate( m4.multiply( q.quatToMat4(cameraTranform.rotation), [0,0,1,1]).slice(0,3) );
-    
+
     //https://stackoverflow.com/questions/7692988/opengl-math-projecting-screen-space-to-world-space-coords
-    let pos = v4.create( 0.0, 0.0, 0.0, 1.0 );
+
     let screenToWorldMatrix = m4.inverse( m4.multiply(perspectiveMatrix,cameraMatrix) );
-    let point = v4.xyz( v4.multiplyVec4Mat4(pos,screenToWorldMatrix) );
+    let pos000 = v4.create( absoulteX, absoulteY, 0.0, 1.0 );
+    let pos001 = v4.create( absoulteX, absoulteY, -0.5, 1.0 );
+    let point000 = v4.xyz( v4.multiplyVec4Mat4(pos000,screenToWorldMatrix) );
+    let point001 = v4.xyz( v4.multiplyVec4Mat4(pos001,screenToWorldMatrix) );
+    let direction = v3.normalize( v3.subtract(point000,point001) ); 
 
     //let output = m4.multiply(transform.getMatrix(),pos);
     if(this.renderCount % 30 == 0){
-      console.log(point);
+      //console.log(point);
       //console.log(absoulteX,absoulteY);
+      //console.log("cameraFront:", roundVec3(cameraFrontVector,1),"\nscreenFront:\n",roundVec3(direction,1));
+   
       positionDirectionPlaneIntersect(
         cameraTranform.position,
-        cameraFrontVector
+        direction
         );
     }
   }
@@ -112,13 +101,14 @@ function positionDirectionPlaneIntersect(position: v3.Vec3,direction: v3.Vec3){
   //const normalizedDirection = v3.normalize(direction);
   let multiplier = Math.abs(position[1] / direction[1]);
   let destination = v3.add(v3.mulScalar(direction,multiplier), position);
-  let roundedDestination = roundVec3(destination);
-  //console.log("x:" + roundedDestination[0] + ", z:" + roundedDestination[2]);
+  let roundedDestination = roundVec3(destination,0);
+  console.log("x:" + roundedDestination[0] + ", y:" + roundedDestination[1] +", z:" + roundedDestination[2]);
 }
 
-function roundVec3(vector:v3.Vec3){
-  let x = Math.round( vector[0] );
-  let y = Math.round( vector[1] );
-  let z = Math.round( vector[2] );
+function roundVec3(vector:v3.Vec3 , decimalPlaces:number){
+  let factor = Math.pow(10,decimalPlaces);
+  let x = Math.round( vector[0] * factor ) / factor;
+  let y = Math.round( vector[1] * factor ) / factor;
+  let z = Math.round( vector[2] * factor ) / factor;
   return v3.create(x,y,z);
 }
