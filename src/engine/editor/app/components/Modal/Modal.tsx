de55a6b1d71511f1102f3f2bee4772255690a07b
Alvin Leung <alvinleung2009@gmail.com>
2021-05-07 15:07:15 -0700

    Added Modal support

---
+++ src/engine/editor/app/components/Modal/Modal.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import ReactDOM from "react-dom";
+import "./Modal.css";
+import { config } from "../../AnimationConfig";
+import { useHotkeys } from "react-hotkeys-hook";
+import { HotkeyConfig } from "../../Hotkeys";
+
+interface Props {
+  children: React.ReactNode | React.ReactNode[];
+  show: boolean;
+  onHide: () => void;
+  width: string | number;
+}
+
+export const Modal = ({ children, show, onHide, width }: Props) => {
+  useHotkeys(
+    HotkeyConfig.ESCAPE,
+    () => {
+      onHide && onHide();
+    },
+    {
+      enableOnTags: ["INPUT", "TEXTAREA"],
+    },
+    []
+  );
+
+  return ReactDOM.createPortal(
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          className={"modal-container typography-base"}
+          initial={{
+            backgroundColor: "rgba(0,0,0,0)",
+          }}
+          animate={{
+            backgroundColor: "rgba(0,0,0,.3)",
+          }}
+          exit={{
+            backgroundColor: "rgba(0,0,0,0)",
+          }}
+          transition={config.DEFAULT_TRANSITION}
+        >
+          <motion.div
+            className="modal-content"
+            style={{
+              width: width,
+            }}
+            initial={{
+              y: 20,
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              boxShadow: "0px 5px 10px 5px rgba(0,0,0,.2)",
+            }}
+            exit={{
+              y: 20,
+              opacity: 0,
+            }}
+            transition={config.DEFAULT_TRANSITION}
+          >
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
+    document.body
+  );
+};
