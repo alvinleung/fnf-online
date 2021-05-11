b2a5eaeb6fc750bb2257929777c294d6e5c11fa5
kwan <sleepmanleung@gmail.com>
2021-05-10 18:04:14 -0700

    Add light Component

---
+++ src/engine/graphics/light/Light.ts
@@ -0,0 +1,54 @@
+import { v3 } from "twgl.js";
+import { Component } from "../../ecs";
+import { EditableComponent, EditableField, Editor } from "../../editor";
+import { v4 } from "../../utils/MatrixUtils";
+
+
+export interface PhongMaterialProperties {
+  specularConstant: number;
+  ambientConstant: number;
+  diffuseConstant: number;
+  shininess: number;
+}
+
+@EditableComponent
+export class LightComponent implements Component{
+  private _isDirectional: boolean;
+  private _color: v3.Vec3;
+  private _intensity: number;
+
+  @EditableField(Editor.RGBA)
+  public set color([r, b, g, a]: v3.Vec3) {
+    this._color = [r, g, b];
+  }
+
+  public get color() {
+    return this._color;
+  }
+
+  @EditableField(Editor.BOOLEAN)
+  public set isDirectional(val: boolean) {
+    this._isDirectional = val;
+  }
+
+  public get isDirectional() {
+    return this._isDirectional;
+  }
+
+  @EditableField(Editor.NUMBER)
+  public set intensity(val: number) {
+    this._intensity = val;
+  }
+
+  public get intensity() {
+    return this._intensity;
+  }
+
+
+}
+
+
+
+
+
+
