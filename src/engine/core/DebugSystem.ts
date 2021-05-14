3ba33a217e3800767f377a81c89d93adbb25c678
kwan <sleepmanleung@gmail.com>
2021-05-13 21:07:36 -0700

    clean up DebugSystem

--- src/engine/core/DebugSystem.ts
+++ src/engine/core/DebugSystem.ts
@@ -30,50 +30,18 @@ export class DebugSystem extends System {
     const cameraEntity = this.camera.entities[0];
     const transform = debugEntity.getComponent(TransformComponent);
     const cameraTranform = cameraEntity.getComponent(TransformComponent);
-
     this.renderCount++;
-
-    const FOV = 54.4;
-    const CLIP_NEAR = 1;
-    const CLIP_FAR = 2000;
     const game = engine as MyGame;
 
-    const clientwidth = game.getCanvas().width;
-    const clientHeight = game.getCanvas().height;
 
-    const perspectiveMatrix = m4.perspective(
-      (FOV * Math.PI) / 180, // field of view
-      clientwidth / clientHeight, // aspect ratio
-      CLIP_NEAR, // nearZ: clip space properties
-      CLIP_FAR // farZ: clip space properties
-    );
+    let scaleX = (Math.sin(this.renderCount / 30) ) ;
+    let scaleY = (Math.sin((this.renderCount + 60) / 30) ) ;
+    let scaleZ = (Math.sin((this.renderCount + 120) / 30) ) ;
 
-    const negativeOffsetPos = v3.negate(cameraTranform.position);
-    const cameraRotMat = m4.inverse(q.quatToMat4(cameraTranform.rotation));
-    const cameraMatrix = m4.translate(cameraRotMat, negativeOffsetPos);
+    //transform.scale = [scaleX,scaleY,scaleZ]
 
-    this.elapsed += delta;
-
-    let absoulteX = (game.input.getAxis("pointerX") / clientwidth) * 2 - 1;
-    let absoulteY = -((game.input.getAxis("pointerY") / clientHeight) * 2 - 1);
-
-    // Vector of the direction the camera is looking at
-    let cameraFrontVector = v3.negate(
-      m4
-        .multiply(q.quatToMat4(cameraTranform.rotation), [0, 0, 1, 1])
-        .slice(0, 3)
-    );
 
     //https://stackoverflow.com/questions/7692988/opengl-math-projecting-screen-space-to-world-space-coords
-
-    let screenToWorldMatrix = m4.inverse(
-      m4.multiply(perspectiveMatrix, cameraMatrix)
-    );
-    let pos000 = v4.create(absoulteX, absoulteY, 0.0, 1.0);
-    let pos001 = v4.create(absoulteX, absoulteY, -0.5, 1.0);
-    let point000 = v4.xyz(v4.multiplyVec4Mat4(pos000, screenToWorldMatrix));
-    let point001 = v4.xyz(v4.multiplyVec4Mat4(pos001, screenToWorldMatrix));
-    let direction = v3.normalize(v3.subtract(point000, point001));
 
     //let output = m4.multiply(transform.getMatrix(),pos);
     if (this.renderCount % 30 == 0) {
@@ -81,7 +49,7 @@ export class DebugSystem extends System {
       //console.log(absoulteX,absoulteY);
       //console.log("cameraFront:", roundVec3(cameraFrontVector,1),"\nscreenFront:\n",roundVec3(direction,1));
       return;
-      positionDirectionPlaneIntersect(cameraTranform.position, direction);
+    
     }
   }
 }
