5a9ab2be3aa20219d133eb37233af65c4a7b1674
Alvin Leung <alvinleung2009@gmail.com>
2021-05-15 15:31:37 -0700

    Established basic editor server

---
+++ EditorServer.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const reload = require("reload");
+
+const ASSET_SHEET_PUBLIC_ACCESS = "/assets-sheet";
+const ASSET_SHEET_PATH = "/src/AssetSheet.json";
+
+const ASSET_PUBLIC_ACCESS = "/assets/";
+const ASSET_FOLDER_PATH = "/assets/";
+
+const app = express();
+const publicDir = path.join(__dirname, "./dist");
+
+// clear console when restart app
+clearConsole();
+
+app.set("port", process.env.PORT || 3000);
+app.use("/", express.static("dist"));
+app.use("/assets", express.static("public"));
+
+app.get("/favicon.ico", (req, res) => {
+  res.status(204);
+  res.end();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicDir, "/index.html"));
+});
+
+app.get(ASSET_SHEET_PUBLIC_ACCESS, (req, res) => {
+  console.log(`${getTimeNow()} Client request asset`);
+  res.sendFile(path.join(__dirname, ASSET_SHEET_PATH));
+});
+
+const server = http.createServer(app);
+
+const serverListen = () => {
+  const serverURL = `http://localhost:${app.get("port")}/`;
+
+  console.log(`Editor server started on ${serverURL}`);
+  console.log(`----------------------------------------------------------`);
+  console.log(`Asset public url      ${serverURL + ASSET_SHEET_PUBLIC_ACCESS}`);
+  console.log(`Asset sheet source    ${ASSET_SHEET_PATH}`);
+  console.log(`Asset source folder   ${ASSET_FOLDER_PATH}`);
+  console.log(`Asset public url      ${serverURL + ASSET_PUBLIC_ACCESS}`);
+  console.log(``);
+  console.log(``);
+};
+
+// Reload code here
+reload(app)
+  .then(function (reloadReturned) {
+    // Reload started, start web server
+    server.listen(app.get("port"), serverListen);
+  })
+  .catch(function (err) {
+    console.error(
+      "Reload could not start, could not start server/sample app",
+      err
+    );
+  });
+
+function getTimeNow() {
+  return new Date().toISOString().match(/(\d{2}:){2}\d{2}/)[0];
+}
+
+function clearConsole() {
+  console.log("\033[2J");
+}
